221529ba0a2532e3f9efee487a05c7f5341e1d7c
YiyiDeng <ivy112014@outlook.com>
2017-03-21 21:28:41 +0800

    1

---
+++ 1-gauss.m
@@ -0,0 +1,26 @@
+function x = gauss(A)
+
+sizeOfA = size(A);
+rowOfA = sizeOfA(1);
+culOfA = sizeOfA(2);
+
+for k = 1 : rowOfA-1
+    for i = k+1 : rowOfA
+        x = A(i,k) / A(k,k);
+        A(i,:) = A(i,:) - x*A(k,:);
+    end
+end
+A
+B = A(:,culOfA);
+x = [];
+x(rowOfA) = B(rowOfA) / A(rowOfA,rowOfA);
+for i = 1:rowOfA-1
+    k = rowOfA-i;
+    s = 0;
+    for j = k+1:rowOfA
+        s = s + A(k,j)*x(j);
+    end
+    x(k) = (B(k) - s) / A(k,k);
+end
+x
+end
